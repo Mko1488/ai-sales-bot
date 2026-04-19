326308ac5022de467d4879988de409c8edbdc87c
Mko1488 <71015523+Mko1488@users.noreply.github.com>
2026-04-19 12:35:31 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+ADMIN_TOKEN = os.getenv("ADMIN_BOT_TOKEN")
+BASE_URL = os.getenv("BASE_URL")
+
+YANDEX_KEY = os.getenv("YANDEX_API_KEY")
+YANDEX_FOLDER = os.getenv("YANDEX_FOLDER_ID")
+
+GOOGLE_SHEET_URL = os.getenv("GOOGLE_SHEET_URL", "")
